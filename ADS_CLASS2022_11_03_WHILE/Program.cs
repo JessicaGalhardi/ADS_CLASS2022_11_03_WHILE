7dae46f5a1e84e801a970d84074787602fafbe20
JessicaGalhardi <104734490+JessicaGalhardi@users.noreply.github.com>
2022-11-03 17:44:51 +0100

    Adicionar arquivos de projeto.

---
+++ ADS_CLASS2022_11_03_WHILE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        try
+        {
+
+            int age = 30;
+
+            while (age >= 18)
+            {
+                Console.WriteLine("With " + age+ " years, the user can vote!"); 
+                age--;
+            }
+
+            if(age<18)
+            {
+                 Console.WriteLine("The user has "+ age+ " years and can't vote!");
+            }
+
+            Console.ReadKey();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error while running the application" + ex);
+
+        }
+    }
+}
